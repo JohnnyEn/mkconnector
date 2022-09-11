2f94892f5ffc3ce45a9c8ce82916771e5a6c4746
Johnny <meatybloodyboy@gmail.com>
2022-09-11 15:04:15 +0200

    master - added check for current logged user notes only

--- src/routines/misskey-watcher.js
+++ src/routines/misskey-watcher.js
@@ -5,12 +5,26 @@ import twitterClient from './twitter-poster.js';
 const MISSKEY_INSTANCE_URL_BASE = process.env.MISSKEY_INSTANCE_URL;
 const MISSKEY_AUTH_KEY = process.env.MISSKEY_INSTANCE_SECRET_KEY;
 
+const getCurrentUser = async () => {
+  const client = new Misskey.api.APIClient({
+    origin: MISSKEY_INSTANCE_URL_BASE,
+    credential: MISSKEY_AUTH_KEY,
+  });
+
+  const currentUser = await client.request('i');
+  return currentUser.id;
+};
+
 const misskeyTimelineWatcher = async () => {
+  const currentUser = await getCurrentUser();
+
   const stream = new Misskey.Stream(MISSKEY_INSTANCE_URL_BASE, { token: MISSKEY_AUTH_KEY }, { WebSocket: WS });
 
   const homeChannel = stream.useChannel('homeTimeline');
   homeChannel.on('note', async (note) => {
-    await twitterClient.postTweet(note);
+    if (note.userId === currentUser) {
+      await twitterClient.postTweet(note);
+    }
   });
 };
 
