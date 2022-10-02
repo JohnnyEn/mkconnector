5946e1c3854f22d21bf7accaf8af3d5d95587318
Johnny <meatybloodyboy@gmail.com>
2022-10-02 15:10:52 +0200

    master - fixed mongodb connection, renamed migration folder

--- src/db/connect.js
+++ src/db/connect.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 
 const databaseName = process.env.NODE_ENV === 'development' ? 'mkconnector_dev' : 'mkconnector';
 
+
 const connect = async () => {
   await mongoose.connect(`mongodb://${process.env.DOCKER_MONGODB_USERNAME}:${process.env.DOCKER_MONGODB_PASSWORD}@mongo:27017`, { dbName: databaseName });
 };
