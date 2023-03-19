44fb44cc4b95d8b424186c5011769acce308a82c
Johnny <meatybloodyboy@gmail.com>
2023-03-19 19:09:07 +0100

    master hotfix - fixed url shortener cause of unhandled fetch fail state

--- src/utils/url-shortener.js
+++ src/utils/url-shortener.js
@@ -1,7 +1,14 @@
 const isGdShortener = async (url) => {
-  return new Promise(async (resolve) => {
+  return new Promise(async (resolve, reject) => {
     let baseUrl = "https://is.gd/create.php?format=simple&url=";
-    const shortUrlResponse = await fetch(baseUrl + encodeURIComponent(url));
+    let shortUrlResponse = null;
+
+    try {
+      shortUrlResponse = await fetch(baseUrl + encodeURIComponent(url));
+    } catch(error) {
+      console.error(error);
+      reject(error);
+    }
 
     resolve(shortUrlResponse.text());
   });
